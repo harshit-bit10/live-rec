1f9a05b97bce37b001666e4370bd4d82fe172fa3
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-04-05 08:45:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ import os
 
 class Config(object):
     # API credentials
-    API_ID = int(os.environ.get("API_ID", ))  # Replace with your actual API ID Ex 152002 don't use inverted commas here
-    API_HASH = os.environ.get("API_HASH", "")  # Replace with your actual API Hash
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")  # Replace with your actual Bot Token
+    API_ID = int(os.environ.get("API_ID",17108931 ))  # Replace with your actual API ID Ex 152002 don't use inverted commas here
+    API_HASH = os.environ.get("API_HASH", "436b24700208cae55ded351d8f25fd7a")  # Replace with your actual API Hash
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7129828945:AAHYgGpRXbLqavREmkiQMDi77Psks3_vGgM")  # Replace with your actual Bot Token
 
     # Owner ID
-    OWNER_ID = int(os.environ.get("OWNER_ID", 6066102279))  # Replace with your actual Owner ID
+    OWNER_ID = int(os.environ.get("OWNER_ID", 5300197778))  # Replace with your actual Owner ID
 
     # Download directory
     DOWNLOAD_DIRECTORY = os.environ.get("DOWNLOAD_DIRECTORY", "./downloads")  # Default download directory
 
     # Optional: Add any other configuration variables you need
-    PM_AUTH_USERS = list(int(x) for x in os.environ.get("PM_AUTH_USERS", "6066102279").split())  # Replace with actual user IDs
-    CHAT_ID = int(os.environ.get("CHAT_ID", -1002414124327))  # Replace with actual group ID
+    PM_AUTH_USERS = list(int(x) for x in os.environ.get("PM_AUTH_USERS", "5300197778").split())  # Replace with actual user IDs
+    CHAT_ID = int(os.environ.get("CHAT_ID", -1002525317083))  # Replace with actual group ID
 
     # Credential text
-    CRED_TEXT = os.environ.get("CRED_TEXT", "SHARKTOONSINDIA")  # Default value for CRED_TEXT, Don't Remove If You Respect The Developer
+    CRED_TEXT = os.environ.get("CRED_TEXT", "@Kushi_Yarige_bedda")  # Default value for CRED_TEXT, Don't Remove If You Respect The Developer
